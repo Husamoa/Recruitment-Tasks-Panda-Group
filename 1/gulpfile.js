95fb3b8949fd43c60a09f9228a4224f5de8f11b6
Husamoa <pawebial@gmail.com>
2019-02-21 13:29:33 +0100

    layout not finished yet

--- 1/gulpfile.js
+++ 1/gulpfile.js
@@ -1,6 +1,9 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/main.scss')
@@ -9,10 +12,23 @@ gulp.task('sass', function() {
             errLogToConsole: true,
             outputStyle: 'expanded'
         }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/css'))
 });
 
+gulp.task('js', () =>
+    gulp.src('src/js/app.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('dist/js'))
+);
+
 gulp.task('watch', function(){
     gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
 });
